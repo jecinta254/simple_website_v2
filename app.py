d72eea3160ee164fedaa43e5b213cdf06b181ab7
jecinta254 <njokijecinta254@gmail.com>
2024-04-09 09:00:16 +0300

    requirements file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -26,6 +26,10 @@ JOBS = [
 @app.route("/")
 def hello_world():
     return render_template('index.html', jobs=JOBS, company_name='Jecinta')
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(debug=True)
